cb9cd736b66a35de1a105838f89ca87a781c3af3
Jeff Norris <Jeff Norris>
2016-12-01 13:42:38 -0800

    initial files

---
+++ Assets/SocketBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+
+public class SocketBehavior : MonoBehaviour
+{
+ 
+  private bool toggle = false;
+
+    private int port = 10999;
+    private TcpServer s;
+
+    void Start()
+    {
+        s = new TcpServer(10, 100);
+        s.Init(ReceiveCompleted);
+        s.Start(new IPEndPoint(IPAddress.Loopback, port));
+    }
+
+    internal void ReceiveCompleted()
+    {
+        toggle = true;
+    }
+
+    void Update()
+    {
+        if (toggle)
+        {
+            Debug.Log("Received.");
+            toggle = false;
+        }
+    }
+}
